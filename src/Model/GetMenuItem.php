430deff92813269547e915f91732bac251f35491
Art <lansky1994@gmail.com>
2021-12-01 23:13:35 +0300

    new3

--- src/Model/GetMenuItem.php
+++ src/Model/GetMenuItem.php
@@ -4,18 +4,20 @@ namespace App\Model;
 
 use App\Interfaces\MenuRoleInterfaces;
 use App\Repository\MenuItemRepository;
+use App\Repository\MenuRepository;
 
 
 class GetMenuItem implements MenuRoleInterfaces
 {
     private $menuItemRepository;
+    private MenuRepository $menuRepository;
 
-    public function __construct(MenuItemRepository $menuItemRepository)
+    public function __construct(MenuItemRepository $menuItemRepository, MenuRepository $menuRepository)
     {
         $this->menuItemRepository = $menuItemRepository;
+        $this->menuRepository = $menuRepository;
     }
 
-    //мы передаем меню айди и в цикле по всем мню айди ищем те которые свзаны по первому (которое мы передали) меню айди
     public function getChildren($id, $menuItems) {
         $childrens = [];
         foreach ($menuItems as $mi) {
@@ -23,16 +25,12 @@ class GetMenuItem implements MenuRoleInterfaces
              $newClidren = [
                 'id' => $mi->getId(),
                 'title' => $mi->getTitle(),
-
             ];
-
                 $child = $this->getChildren($mi->getID(), $menuItems);
 
                 if (count($child)) {
                     $newClidren['children'] = $child;
                 }
-
-
                 $childrens[] = $newClidren;
             }
         }
@@ -42,77 +40,36 @@ class GetMenuItem implements MenuRoleInterfaces
     public function getMenuRole($alias, $id)
     {
         $menuItems = $this->menuItemRepository->findAllCoursByProject($alias, $id);
+        $menu = $this->menuRepository->findBy(['alias' => $alias, 'showMenu' => true]);
 
         $resp = [];
 
-        // Ищем меню айтем у которого есть родитель
-        foreach ($menuItems as $menuItem){
-            if ($menuItem->getMenuItem() == null)
-            {
-                $isSet = false;
-                $sub = [
-                    "id" => $menuItem->getId(),
-                    "title" => $menuItem->getTitle(),
-                    "alias" => $menuItem->getAlias(),
+        foreach ($menu as $m){
 
-                ];
-                $childrens = $this->getChildren($menuItem->getId(), $menuItems);
+            $firstMenu = [
+                "id" => $m->getId(),
+                "title" => $m->getTitle(),
+                "alias" => $m->getAlias(),
+            ];
 
-                if (count($childrens)) {
-                    $sub['children'] =  $childrens;
-                }
-
-                // Нет ли у нас поля в таблице меню проверяем
-                foreach ($resp as $index => $value) {
-                    if ($value["id"] == $menuItem->getParent()->getId())
-                    {
-                        $isSet = true;
-                        $resp[$index]["children"][] = $sub;
-                    }
-                }
-
-                //Создаем родителя
-                if (!$isSet)
+            foreach ($menuItems as $menuItem){
+                if ($menuItem->getMenuItem() == null && $menuItem->getParent()->getId() == $m->getId())
                 {
-                    $resp[] = [
-                        "id" => $menuItem->getParent()->getId(),
-                        "title" => $menuItem->getParent()->getTitle(),
-                        "alias" => $menuItem->getParent()->getAlias(),
-                        "children" => [$sub]
+                    $sub = [
+                        "id" => $menuItem->getId(),
+                        "title" => $menuItem->getTitle(),
+                        "alias" => $menuItem->getAlias(),
                     ];
+                    $childrens = $this->getChildren($menuItem->getId(), $menuItems);
+
+                    if (count($childrens)) {
+                        $sub['children'] =  $childrens;
+                    }
+                    $firstMenu['children'][] = $sub;
                 }
             }
+            $resp[] = $firstMenu;
         }
-        if (empty($resp)){
-            $resp[] = [
-                "id" => $menuItem->getParent()->getId(),
-                "title" => $menuItem->getParent()->getTitle(),
-                "alias" => $menuItem->getParent()->getAlias(),
-            ];
-            return $resp;
-        }
-
-//        //Смотрим подпункты у которых нет меню айди -> родителя
-//        foreach ($menuItems as $menuItem) {
-//
-//            if ($menuItem->getParent() == null)
-//            {
-//                //Засовываем под меню в под меню
-//                foreach ($resp as $key=>$value) {
-//                    foreach ($value["children"] as $j=>$c) {
-//                        if ($c["id"] == $menuItem->getMenuItem()->getId()) {
-//                            $sub = [
-//                                "id" => $menuItem->getId(),
-//                                "title" => $menuItem->getTitle(),
-//                                "alias" => $menuItem->getAlias(),
-//                            ];
-//
-//                            $resp[$key]["children"][$j]["children"][]=$sub;
-//                        }
-//                    }
-//                }
-//            }
-//        }
         return $resp;
     }
 
